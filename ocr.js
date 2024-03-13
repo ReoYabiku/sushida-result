109ae5cb2dd9fa039e3cf10b17960bd288ba77f4
ReoYabiku <alcremikyu.z@gmail.com>
2024-03-14 03:20:15 +0900

    introduce tesseract.js but cannot import cdn because content security policy

--- ocr.js
+++
@@ -1,7 +0,0 @@
-const canvas = document.querySelector("[id='#canvas']");
-
-canvas.toBlob((blob) => {
-	const url = URL.createObjectURL(blob);
-	console.log(url);
-})
-
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { createWorker } from 'tesseract.js';
+
 async function getTabId() {
 	const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 	return tab.id;
@@ -6,7 +8,23 @@ async function getTabId() {
 chrome.scripting
 	.executeScript({
 		target: {tabId: await getTabId()},
-		files: ["ocr.js"],
+		func: () => {
+			const canvas = document.querySelector("[id='#canvas']");
+			console.log("in the function");
+
+			canvas.toBlob((blob) => {
+				const url = URL.createObjectURL(blob);
+
+				(
+					async () => {
+						const worker = await createWorker('eng');
+						const ret = await worker.recognize(url);
+						console.log(ret.data.text);
+						await worker.terminate();
+					}
+				)();
+			})
+		}
 	})
 	.then(console.log("script injected"));
 
